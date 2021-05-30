6505b3f5e9bb8eb9ac6badb3e69332c1bd23dd70
Dew2118 <kavin2118@gmail.com>
2021-05-30 09:16:33 +0700

    make display consisten with curses display as far as parameter goes

--- src/display.py
+++ src/display.py
@@ -1,15 +1,13 @@
 class Display:
     """Display game using normal print."""
-    # TODO: make class interface consistent between Display and Curses_display
+    # DONE: make class interface consistent between Display and Curses_display
     # __init__ should not take game as parameter
     # but game object will be passed when call display method.
-    def __init__(self, game) -> None:
-        self.game = game
 
-    def display(self):
-        for hand in self.game.all_hand:
+    def display(self, game):
+        for hand in game.all_hand:
             print(f'{hand.name} \n')
-            if hand.name == 'dealer' and self.game.current_hand.name != 'dealer':
+            if hand.name == 'dealer' and game.current_hand.name != 'dealer':
                 print(hand.cards[0].value, hand.cards[1].suit, '\n')
             else:
                 for c in hand.cards:
